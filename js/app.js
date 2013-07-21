552f4140e0e31ab88db5d0b5e0ecc5f031f44f14
xmichaelx <michal.drzal@gmail.com>
2013-07-21 11:37:14 -0400

    Code divided into modules for better management.

--- js/app.js
+++ js/app.js
@@ -11,95 +11,16 @@ window.require(
 		"dojo/ready", "dojo/dom","dijit/registry", "dojo/_base/declare", 
 		"dgrid/OnDemandGrid", "dgrid/CellSelection", "dgrid/Keyboard", "dgrid/extensions/ColumnResizer", 
 		"dgrid/Selection","dgrid/extensions/DijitRegistry","dgrid/editor","dgrid/tree",
-		  "dijit/layout/ContentPane", "dojo/aspect"
+		"dijit/layout/ContentPane", "dojo/aspect", "customPanes/dataPane", "customPanes/configurationPane"
 	], 
 	function( ready, dom, registry, declare, 
 			OnDemandGrid, CellSelection, Keyboard, ColumnResizer, Selection, DijitRegistry, editor, tree,
-			ContentPane, aspect) {
+			ContentPane, aspect,dataPane,configurationPane) {
      ready(function(){
-        // logic that requires that Dojo is fully initialized should go here
-        // template for all grids used in the app
-        // custom grid is obtained via mixins, which add required functionality to plain OnDemandGrid
-        var CustomGrid = declare([OnDemandGrid,  Selection, Keyboard, ColumnResizer, DijitRegistry]);
-
 		// processing done when dataPane is fully loaded
-		registry.byId("dataPane").set("onDownloadEnd", function () { 
-			// statistics grid displays metadata concerning currently selected attribute
-			 // attributes grid displays list of attributes and the order they appear in the events
-			
-			var attributesGrid = new CustomGrid({
-				columns: { id : {label: "#"}, name: {label: "Attribute name"} },
-			    store : attributesStore
-			},"attributesGrid");
-            console.log("Finished creating attributes grid");
-            
-			var statisticsGrid = new CustomGrid({
-				columns: { id : {label: "Info"}, value: {label: "Value"} },
-				store: statisticsStore
-			}, "statisticsGrid");
-			console.log("Finished creating statistics grid");
-			
-			
-			aspect.after(attributesStore, "setData", function() { registry.byId("attributesGrid").refresh();});
-			console.log("Added updates after changes in attributesStore");
-			// when we select attribute, we display info in a grid
-			attributesGrid.on("dgrid-select", function(event){
-			    var data = [{id:"Domain", value:event.rows[0].data.domain},{id:"Parameters",value: event.rows[0].data.parameters}];
-			    statisticsStore.setData(data);
-			    statisticsGrid.refresh();
-			    console.log("Refreshed statistics grid");
-			});
-			
-			attributesGrid.on("dgrid-deselect", function(event){
-			    statisticsStore.setData([]);
-			    statisticsGrid.refresh();
-			    console.log("Cleared statistics grid");
-			});
-			// we destroy old grid with data
-			aspect.before(eventsStore, "setData", function() { registry.byId("raw_data").destroyDescendants(true);console.log("cleared events grid"); });
-			console.log("Added updates before changes in eventsStore");
-			// we destroy old grid with data
-			aspect.after(eventsStore, "setData", function() { 
-				var eventsGrid = new CustomGrid(
-				{ 
-					store : eventsStore, 
-					columns : attributesStore.query({}).map(function(x) { return {field : x.name, label:x.name};})
-				});
-				registry.byId("raw_data").addChild(eventsGrid); 
-				console.log("Created new event grid");
-			});
-			console.log("Added updates after changes in eventsStore");
-		});
+		registry.byId("dataPane").set("onDownloadEnd", dataPane.setup);
 		// processing done when configurationPane is fully loaded
-		registry.byId("configurationPane").set("onDownloadEnd", function () { 
-			var runsGrid = new CustomGrid({
-				columns: { id : {label: "Run"}, value: editor({name: "CheckBox",label: "Selected", field: "selected",  autoSave : true}, "checkbox") },
-				store: runsStore, autosave: true
-	            }, "runsGrid");
-			console.log("Created runs grid");
-			aspect.after(runsStore, "setData", function() { registry.byId("runsGrid").refresh();});
-			
-			// we destroy old tabs and grids with configuration data
-			aspect.before(runsStore, "setData", function() { 
-				registry.byId("runTabs").getChildren().forEach(function (child) {
-					registry.byId("runTabs").removeChild(child); registry.byId(child.id).destroy(); 
-				});
-				console.log("Removed old tabs");
-			});
-			// we create new tabs and grids containing newly loaded data
-			aspect.after(runsStore, "setData", function() { 
-				runsStore.query({}).forEach(function (run) {
-						var contentPane = new ContentPane({ title: run.id });
-						var grid = new CustomGrid({
-						columns: { name : {label: "#"}, value: {label: "Value"} },
-			            store : parametersStore, query : { parent : run.id}});
-			            
-						contentPane.addChild(grid);
-						registry.byId("runTabs").addChild(contentPane);
-				});
-				console.log("Created new tabs");
-			});
-		});
+		registry.byId("configurationPane").set("onDownloadEnd", configurationPane.setup);
 		
 		dom.byId("load_files").addEventListener('change', 
 		function  (evt) {
