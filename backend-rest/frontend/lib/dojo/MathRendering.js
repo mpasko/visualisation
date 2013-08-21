54aa8bc427ae6a34115325f625e38215ccaa495b
mpasko256 <mpasko256@gmail.com>
2013-08-21 23:48:13 +0200

    New Feature: sets formatting: range and list

--- backend-rest/frontend/lib/dojo/MathRendering.js
+++ backend-rest/frontend/lib/dojo/MathRendering.js
@@ -1,21 +1,41 @@
 /* global define window */
 /* global define console */
 
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+
 window.define([], function() {
     return {
         renderOutput : function(id, hypotheses) {
                 console.log("The server returned: ", hypotheses);
                 var output = "";
                 var dict = {">" : ">", "<" : "<", ">=":"\\geq","<=":"\\leq","=":"="};
+				var renderSelector = function(selector) {
+					if(!isEmpty(selector.range_begin)&&!isEmpty(selector.range_end)){
+						return ["(",selector.name,"\\in\\langle",selector.range_begin,"\\ ",selector.range_end,"\\rangle",")"].join(" ");
+					} else if (!isEmpty(selector.set_elements)){
+						var index = 0;
+						output = ["(",selector.name,"\\in\\{"];
+						selector.set_elements.forEach(function(element){
+							output = output.join(element);
+							++index;
+							if(index!=selector.set_elements.length){
+								output = output.join(",\\ ");
+							}
+						});
+						return output.join("\\})").join(" ");
+					}else{
+						return ["(",selector.name,dict[selector.comparator],selector.value,")"].join(" ");
+					}
+                };
                 hypotheses.forEach(function (hyp) {
                     var out = hyp.classes[0];
-                    var second = ["(",out.name,dict[out.comparator],out.value,")"].join(" ");
+                    var second = renderSelector(out);
                     
                     hyp.rules.forEach(function(rule) {
                         console.log(rule.selectors);
-                        var first = rule.selectors.map(function(selector) {
-                            return ["(",selector.name,dict[selector.comparator],selector.value,")"].join(" ");
-                        }).join(" \\land ");
+                        var first = rule.selectors.map(renderSelector).join(" \\land ");
                         output = output.concat(["$$",first,"\\implies",second,"$$"].join(" "));
                         
                     });
--- backend-rest/src/main/java/agh/aq21gui/model/output/ClassDescriptor.java
+++ backend-rest/src/main/java/agh/aq21gui/model/output/ClassDescriptor.java
@@ -6,6 +6,8 @@ package agh.aq21gui.model.output;
 
 import agh.aq21gui.aq21grammar.TParser;
 import agh.aq21gui.utils.TreeNode;
+import java.util.LinkedList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -21,6 +23,13 @@ public class ClassDescriptor {
 	public String comparator;
 	@XmlElement
 	public String value;
+	@XmlElement
+	public String range_begin="";
+	@XmlElement
+	public String range_end="";
+	@XmlElement
+	public List<String> set_elements=null;
+	
 	
 	@Override
 	public String toString(){
@@ -38,16 +47,21 @@ public class ClassDescriptor {
 		comparator = desc.childAt(1, TParser.EQUAL).value();
 		StringBuilder builder = new StringBuilder();
 		if(desc.tree().getChild(2).getType()==TParser.RANGE){
+			
 			TreeNode range = desc.childAt(2, TParser.RANGE);
-			builder.append(range.childAt(0, TreeNode.ANY_TYPE).value());
+			range_begin = range.childAt(0, TreeNode.ANY_TYPE).value();
+			range_end = range.childAt(1, TreeNode.ANY_TYPE).value();
+			builder.append(range_begin);
 			builder.append("..");
-			builder.append(range.childAt(1, TreeNode.ANY_TYPE).value());
+			builder.append(range_end);
 			value = builder.toString();
 		}else if(desc.tree().getChild(2).getType()==TParser.VALUE_SET){
 			TreeNode set = desc.childAt(2, TParser.VALUE_SET);
 			int count = set.tree().getChildCount();
+			set_elements = new LinkedList<String>();
 			for(TreeNode itemNode : set.iterator(TParser.ID)){
 				builder.append(itemNode.value());
+				set_elements.add(itemNode.value());
 				--count;
 				if(count>0){
 					builder.append(",");
